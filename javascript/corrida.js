ec9a2bd53dc72fcc0491cf42396260572e2e60d3
Kelvys Moura <kelvysmoura2@gmail.com>
2024-04-03 21:42:19 -0300

    Corrida de carros

---
+++ javascript/corrida.js
@@ -0,0 +1,27 @@
+
+const carros = [
+    { name: "Carro 1", velocidade: 0, distancia: 0 },
+    { name: "Carro 2", velocidade: 0, distancia: 0 },
+    { name: "Carro 3", velocidade: 0, distancia: 0 }
+];
+
+function atualizarPosicao() {
+
+    let vencedor;
+
+    carros.forEach(function (carro) {
+        carro.distancia += carro.velocidade + parseInt(Math.random() * 10 + 10);
+        if(!vencedor && carro.distancia >= 100) {
+            vencedor = carro.name;
+        }
+        console.log(`${carro.name} - Distancia percorrida: ${carro.distancia} metros`);
+    });
+
+    if(vencedor) {
+        console.log(`${vencedor} venceu a corrida!`);
+        clearInterval(interval);
+    }
+    
+}
+
+const interval = setInterval(atualizarPosicao, 1000);
