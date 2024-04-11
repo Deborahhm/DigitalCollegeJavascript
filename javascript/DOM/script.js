8c9852462afb841967b6342b9f10b3fc93803682
Kelvys Moura <kelvysmoura2@gmail.com>
2024-04-10 21:45:12 -0300

    Adicionando e lenado localStorage dinamicamente

--- javascript/DOM/script.js
+++ javascript/DOM/script.js
@@ -7,11 +7,36 @@ let inputPriceError = document.getElementById('input-price-error');
 let formMsg = document.getElementById('form-msg');
 let productList = document.getElementById('product-list');
 
+let storage = window.localStorage.getItem('products');
+storage = JSON.parse(storage);
+
+storage.forEach(function (item, index) {
+    let trNode = document.createElement('tr');
+    let tdName = document.createElement('td');
+    let tdPrice = document.createElement('td');
+    let tdAction = document.createElement('td');
+    
+    let editLink = document.createElement('a');
+    editLink.href = "#edit";
+    editLink.textContent = "Editar ";
+    
+    let deleteLink = document.createElement('a');
+    deleteLink.href = "#delete";
+    deleteLink.textContent = "Excluir ";
+    deleteLink.dataset.index = index;
+
+    tdAction.append(editLink, deleteLink);
+
+    tdName.textContent = item.name;
+    tdPrice.textContent = item.price;
+    trNode.append(tdName, tdPrice, tdAction);
+    productList.querySelector('tbody').prepend(trNode);
+})
+
+console.log(storage);
 
 formProdutos.addEventListener('submit', event => {
     event.preventDefault();
-
-    
 
     inputNameError.textContent = '';
     inputPriceError.textContent = '';
@@ -33,8 +58,14 @@ formProdutos.addEventListener('submit', event => {
         return;
     }
 
-    window.localStorage.setItem('product-name', inputName.value);
-    window.localStorage.setItem('product-price', inputPrice.value);
+    let product = {
+        name: inputName.value,
+        price: inputPrice.value
+    }
+
+    storage.push(product);
+
+    window.localStorage.setItem('products', JSON.stringify(storage));
     
     let trNode = document.createElement('tr');
     let tdName = document.createElement('td');
